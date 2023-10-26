52e2109edff5b1aa93ab1a8c27b04cf0313455bd
jnickla1 <61997641+jnickla1@users.noreply.github.com>
2023-10-26 17:57:51 -0400

    out one directory

--- install_postprocessor.sh
+++ install_postprocessor.sh
@@ -39,9 +39,9 @@ git clone https://github.com/NCAR/CESM_postprocessing
 cd CESM_postprocessing
 
 make
-mv ../macCESMpostprocess/utley_modules Machines/
+mv ../../macCESMpostprocess/utley_modules Machines/
 sed -i '' "s|/Users/jnicklas1/cesmppp/CESM_postprocessing|${PWD}|g" Machines/utley_modules
-mv ../macCESMpostprocess/machine_postprocess.xml Machines/
+mv ../../macCESMpostprocess/machine_postprocess.xml Machines/
 PYTHONROOT=$(which python)
 envPYTHONRT=${PYTHONROOT%/*}
 envROOT=${envPYTHONRT%/*}
@@ -53,7 +53,7 @@ sed -i '' "s|python3.12|python${PYverC}|g" Machines/machine_postprocess.xml
 ./create_python_env -machine utley
 
 echo "Making a few post-install fixes"
-mv ../macCESMpostprocess/activate_this.py cesm-env2/bin
+mv ../../macCESMpostprocess/activate_this.py cesm-env2/bin
 sed -i '' "657s|aleph|utley|g" cesm_utils/cesm_utils/create_postprocess.py
 sed -i '' "34s|. activate|eval \"\$(conda shell.bash hook)\"|g" Templates/postprocess.tmpl
 sed -i '' "35s|^$|conda activate \${PWD%/*}|g" Templates/postprocess.tmpl
