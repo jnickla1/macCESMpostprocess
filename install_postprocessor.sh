b12b3c66d946ed50d08cbb5fd06aa4cfdb703d2f
jnickla1 <61997641+jnickla1@users.noreply.github.com>
2023-10-26 18:33:18 -0400

    Update install_postprocessor.sh to have cp instead of mv

--- install_postprocessor.sh
+++ install_postprocessor.sh
@@ -40,9 +40,9 @@ git clone https://github.com/NCAR/CESM_postprocessing
 cd CESM_postprocessing
 
 make
-mv ../../macCESMpostprocess/utley_modules Machines/
+cp ../../macCESMpostprocess/utley_modules Machines/
 sed -i '' "s|/Users/jnicklas1/cesmppp/CESM_postprocessing|${PWD}|g" Machines/utley_modules
-mv ../../macCESMpostprocess/machine_postprocess.xml Machines/
+cp ../../macCESMpostprocess/machine_postprocess.xml Machines/
 PYTHONROOT=$(which python)
 envPYTHONRT=${PYTHONROOT%/*}
 envROOT=${envPYTHONRT%/*}
@@ -54,7 +54,7 @@ sed -i '' "s|python3.12|python${PYverC}|g" Machines/machine_postprocess.xml
 ./create_python_env -machine utley
 
 echo "Making a few post-install fixes"
-mv ../../macCESMpostprocess/activate_this.py cesm-env2/bin
+cp ../../macCESMpostprocess/activate_this.py cesm-env2/bin
 sed -i '' "657s|aleph|utley|g" cesm_utils/cesm_utils/create_postprocess
 sed -i '' "34s|. activate|eval \"\$(conda shell.bash hook)\"|g" Templates/postprocess.tmpl
 sed -i '' "35s|^$|conda activate \${PWD%/*}|g" Templates/postprocess.tmpl
