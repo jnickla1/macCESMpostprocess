a1a90bbb4381cbb4928ea6011758cd56444a1f0f
jnickla1 <61997641+jnickla1@users.noreply.github.com>
2023-11-21 01:17:42 -0500

    Fix pyAverager script

--- install_postprocessor.sh
+++ install_postprocessor.sh
@@ -62,8 +62,16 @@ sed -i '' "s|/Users/jnicklas1/miniforge3/envs/cesm-env0|${envROOT}|g" Machines/m
 echo "Making a few post-install fixes"
 cp ../../macCESMpostprocess/activate_this.py cesm-env2/bin
 sed -i '' "654s|aleph|utley|g" cesm_utils/cesm_utils/create_postprocess
+#need our own template
 sed -i '' "34s|. activate|eval \"\$(conda shell.bash hook)\"|g" Templates/postprocess.tmpl
 sed -i '' "35s|^$|conda activate \${PWD%/*}|g" Templates/postprocess.tmpl
-sed -i '' "s|options.caseroot\[0\]|options.caseroot|g" timeseries/timeseries/cesm_tseries_generator.py
 cp Templates/batch_aleph.tmpl Templates/batch_utley.tmpl
+#timeseries stuff - caseroot is a scalar
+sed -i '' "s|options.caseroot\[0\]|options.caseroot|g" timeseries/timeseries/cesm_tseries_generator.py
+#averager stuff - deal with scalar metavariables
+unzip cesm-env2/lib/python3.11/site-packages/PyAverager-0.9.17-py3.11.egg #unzip pyAverager
+mv cesm-env2/lib/python3.11/site-packages/PyAverager-0.9.17-py3.11.egg ../../macCESMpostprocess/PyAverager-0.9.17-py3.11.egg-old
+sed -i '' "623s|len(in_meta) > 0:|(np.ndim(in_meta)==0): out_meta[()]==in_meta[()]|g" cesm-env2/lib/python3.11/site-packages/pyaverager/climFileIO.py
+sed -i '' "624s|    o|elif (len(in_meta) > 0): o|g" cesm-env2/lib/python3.11/site-packages/pyaverager/climFileIO.py
+
 echo "Install Complete"
